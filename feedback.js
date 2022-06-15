b93eac254db767f6d84e1e9a0e9ffe094e6d20aa
Sai Kumar Avula <88418659+saikumars78@users.noreply.github.com>
2022-06-15 12:22:07 +0530

    Add files via upload

---
+++ feedback.js
@@ -0,0 +1,8 @@
+
+let subEl=document.getElementById("sub-el")
+let aboutEl=document.getElementById("about-el")
+let abou='we appreciate you taking the time to give a rating.If you ever need more support dont hesitate to get in touch with us'
+function submit(){
+    aboutEl.textContent=abou
+
+}
